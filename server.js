cfa7d7ef551f423595fd8970c4a517417883783d
mohitjeenast <tech@levithan.tech>
2025-12-05 15:24:13 +0530

    uploading images

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+
+
 const app = express();
 
 // Middleware
@@ -15,7 +17,9 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-app.use(express.json());
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 app.use('/api/questions', require('./routes/questions'));
