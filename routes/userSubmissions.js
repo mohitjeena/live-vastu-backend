76e877f14bcbd9d38b0a51585f88120a9463cf65
mohitjeenast <tech@levithan.tech>
2025-12-10 12:19:18 +0530

    saved report in db

--- routes/userSubmissions.js
+++ routes/userSubmissions.js
@@ -212,54 +212,29 @@ router.post("/save-report", async (req, res) => {
     try {
         const { session_id, reportHtml } = req.body;
 
+        
+        if (!session_id || !reportHtml) {
+            return res.status(400).json({
+                success: false,
+                message: "session_id and reportHtml are required"
+            });
+        }
+
     const user = await UserSubmission.findOne({ session_id });
      
         
-    if (!user) {
-        return res.json({ success: false });
-    }
+     if (!user) {
+            return res.json({ success: false, message: "User not found" });
+        }
 
-        // Convert HTML to PDF
-        const file = { content: reportHtml };
-        const pdfBuffer = await pdf.generatePdf(file, {
-            format: "A4",
-            printBackground: true,
-            margin: {
-                top: "20px",
-                bottom: "20px",
-                left: "20px",
-                right: "20px"
-            }
-        });
+     user.vastu_report  = reportHtml;
+     await user.save();
 
-        // Send Email
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: "mjeenaalm09@gmail.com",
-                pass: "hnhzxtmxlatlleoj",
-            },
-        });
-
-        const mailOptions = {
-            from: "mjeenaalm09@gmail.com",
-            to: user.customer_email || 'mjeenaalm01@gmail.com',
-            subject: "Your Vastu Report",
-            text: "Please find your PDF report attached.",
-            attachments: [
-                {
-                    filename: "report.pdf",
-                    content: pdfBuffer,
-                    contentType: "application/pdf"
-                }
-            ]
-        };
-
-        await transporter.sendMail(mailOptions);
+        
 
         res.json({
             success: true,
-            message: "Email sent successfully!"
+            message: "Report saved successfully"
         });
 
     } catch (err) {
