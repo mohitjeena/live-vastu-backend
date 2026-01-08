a1abcd0c93485c27a03469dc3e1d18df505be912
mohitjeenast <tech@levithan.tech>
2026-01-08 11:09:28 +0530

    added secret key

--- server.js
+++ server.js
@@ -33,6 +33,17 @@ app.use(cors({
   credentials: true
 }));
 
+
+app.use((req, res, next) => {
+  const key = req.headers["x-site-key"];
+
+  if (key !== process.env.SITE_SECRET_KEY) {
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  next();
+});
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
