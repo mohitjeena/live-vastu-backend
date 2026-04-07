8c989ed97a178822d44885f29a21a11cc4d781f2
mohitjeena <mjeenaalm09@gmail.com>
2026-04-07 16:29:05 +0530

    make route for fetching user details

---
+++ routes/userDetailsRoute.js
@@ -0,0 +1,34 @@
+const express=  require("express")
+const router = express.Router()
+const UserDetails = require("../models/userDetails")
+
+
+router.get('/:id',async (req,res)=>{
+    try {
+        const { id } = req.params;
+       const exist =  await UserDetails.findById(id)
+       if(exist)
+       {
+        res.status(200).json({
+            message: "details found",
+            data: exist,
+            success: true
+        })
+       }
+       else{
+            res.status(404).json({
+            message: "details not found",
+            success: false
+        })
+       }
+        
+    } catch (error) {
+        console.log("error while fetching user details ",error)
+        res.status(500).json({
+            success:false,
+            message: "internal server error"
+        })
+    }
+})
+
+module.exports = router;
